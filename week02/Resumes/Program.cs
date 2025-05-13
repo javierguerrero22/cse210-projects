478975b80e6dfb6abfa7dc6a3aaeaa4b426a6e6d
javierguerrero22 <javierguerrero22jg@gmail.com>
2025-05-13 11:58:27 -0400

    commited

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._company = "javier's";
+        job1._jobTitle = "software developer";
+        job1._startYear = 2007;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+
+        job2._company = "somewhere's";
+        job2._jobTitle = "software engineer";
+        job2._startYear = 2000;
+        job2._endYear = 2025;
+
+        // job1.Display();
+        // job2.Display();
+        Resume myresume = new Resume();
+        myresume._name = "Javier Guerrero";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+
+        myresume.Display();
     }
 }
